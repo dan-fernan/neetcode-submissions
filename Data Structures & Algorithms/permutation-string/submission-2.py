165f2c7a0028be5e824bbcff5c25ba52ea29df98
Daniel Fernandez <danielfernandez.ny@gmail.com>
2026-08-09 18:58:25 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1): 
+            return False
+        s1_arr_cnt = [0] * 26
+        running_arr_cnt = [0] * 26
+        
+        for i in range(len(s1)):
+            s1_arr_cnt[ord(s1[i]) - 97] += 1
+            running_arr_cnt[ord(s2[i]) - 97] += 1
+        
+        key = '#'.join([str(x) for x in s1_arr_cnt])
+        if key == '#'.join([str(x) for x in running_arr_cnt]): 
+            return True
+        
+        left = 0
+        for right in range(len(s1), len(s2)):
+            running_arr_cnt[ord(s2[left]) - 97] -= 1
+            running_arr_cnt[ord(s2[right]) - 97] += 1
+            left += 1
+            curr_key = '#'.join([str(x) for x in running_arr_cnt])
+
+            if curr_key == key:
+                return True
+        return False
+
+            
